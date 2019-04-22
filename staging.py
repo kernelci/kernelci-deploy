94049e72c22f2d2fab6ed2bccf1f9184fb236d4e
Guillaume Tucker <guillaume.tucker@collabora.com>
2019-04-22 14:00:26 +0100

    staging.py: simplify command line options with projects dict
    Add a dictionary with the settings to use for each KernelCI project
    (kernelci-core, kernelci-backend, kernelci-frontend) to only require
    one command line parameter with the project name.  Keep the
    shell-expanded list of patches as this may be done at the discretion
    of the user running the script.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- staging.py
+++ staging.py
@@ -27,27 +27,38 @@ import sys
 import urllib
 
 GITHUB_API = "https://api.github.com/"
-KERNELCI_CORE_URL = "https://github.com/kernelci/kernelci-core.git"
+
+PROJECTS = {
+    'kernelci-core': {
+        'url': "https://github.com/kernelci/kernelci-core.git"
+    },
+    'kernelci-backend': {
+        'url': "https://github.com/kernelci/kernelci-backend.git"
+    },
+    'kernelci-frontend': {
+        'url': "https://github.com/kernelci/kernelci-frontend.git"
+    },
+}
 
 
 def shell_cmd(cmd):
     subprocess.check_output(cmd, shell=True)
 
 
-def checkout_repository(args):
-    if not os.path.exists(args.path):
+def checkout_repository(path, project):
+    if not os.path.exists(path):
         shell_cmd("git clone {url} {path}".format(
-            url=args.repo_url, path=args.path))
+            url=project.url, path=path))
     shell_cmd("""\
 cd {path}
 git reset --hard --merge
 git fetch origin master
 git checkout FETCH_HEAD
-""".format(path=args.path))
+""".format(path=path))
 
 
 def get_pull_requests(args):
-    path = '/'.join(['repos', args.project, args.repo, 'pulls'])
+    path = '/'.join(['repos', args.namespace, args.project, 'pulls'])
     base_url = urllib.parse.urljoin(GITHUB_API, path)
     url_params = urllib.parse.urlencode({'state': 'open'})
     url = '?'.join([base_url, url_params])
@@ -57,13 +68,13 @@ def get_pull_requests(args):
     return data
 
 
-def pull(args, pr):
+def pull(args, pr, path):
     head = pr['head']
     try:
         shell_cmd("""\
 cd {path}
 git pull --no-ff --no-edit {origin} {branch}
-""".format(path=args.path,
+""".format(path=path,
            origin=head['repo']['clone_url'],
            branch=head['ref']))
     except subprocess.CalledProcessError:
@@ -71,29 +82,29 @@ git pull --no-ff --no-edit {origin} {branch}
         shell_cmd("""\
 cd {path}
 git reset --merge
-""".format(path=args.path))
+""".format(path=path))
         return False
     return True
 
 
-def apply_patches(args):
+def apply_patches(args, path):
     for patch in args.patches:
         print("Applying patch: {}".format(patch))
         try:
             shell_cmd("""\
 cat {patch} | (cd {path} && git am)
-""".format(path=args.path, patch=patch))
+""".format(path=path, patch=patch))
         except subprocess.CalledProcessError:
             print("WARNING: Failed to apply patch")
             shell_cmd("""\
 cd {path}
 git am --abort
-""".format(path=args.path))
+""".format(path=path))
             return False
     return True
 
 
-def create_tag(args):
+def create_tag(args, path):
     tag = args.tag or "staging-{}".format(
         datetime.date.today().strftime('%Y%m%d'))
     print("Tag: {}".format(tag))
@@ -101,41 +112,41 @@ def create_tag(args):
 cd {path}
 git tag -l | grep {tag} && git tag -d {tag}
 git tag -a {tag} -m {tag}
-""".format(path=args.path, tag=tag))
+""".format(path=path, tag=tag))
     return tag
 
 
 def push_tag_and_branch(args, tag):
-    print("Function not implemented: push_tag_and_branch()")
+    print("WARNING - Function not implemented: push_tag_and_branch()")
 
 
 def main(args):
-    checkout_repository(args)
+    path = os.path.join('checkout', args.project)
+    project = PROJECTS.get(args.project)
+    checkout_repository(path, project)
     prs = get_pull_requests(args)
     for pr in prs:
-        pull(args, pr)
-    if not apply_patches(args):
+        pull(args, pr, path)
+    if not apply_patches(args, path):
         print("Aborting, all patches must apply.")
         return False
-    tag = create_tag(args)
-    push_tag_and_branch(args, tag)
+    tag = create_tag(args, path)
+    # push_tag_and_branch(args, tag)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Create staging.kernelci.org branches")
-    parser.add_argument("path",
-                        help="Path to the kernelci-core checkout")
+    parser.add_argument("project", choices=PROJECTS.keys(),
+                        help="Name of the Github project")
     parser.add_argument("patches", nargs='*',
                         help="Path to extra patches to apply")
+    parser.add_argument("--path",
+                        help="Path to the local checkout, default is $PWD")
     parser.add_argument("--tag",
                         help="Tag to create, default is to use current date")
-    parser.add_argument("--project", default="kernelci",
-                        help="Name of the Github project")
-    parser.add_argument("--repo", default="kernelci-core",
-                        help="Name of the Github repository")
-    parser.add_argument("--repo-url", default=KERNELCI_CORE_URL,
-                        help="URL of the Github repository")
     parser.add_argument("--branch", default="staging.kernelci.org",
                         help="Name of the branch to force-push to")
+    parser.add_argument("--namespace", default='kernelci',
+                        help="Github project namespace")
     args = parser.parse_args(sys.argv[1:])
     main(args)
