48bc5486a46f0e085fa4f44e1f1f18ad2bf5f659
Guillaume Tucker <guillaume.tucker@collabora.com>
2019-07-11 10:59:24 +0100

    kernel.py: add script to create a kernel test branch
    Add kernel.py to create a kernel test branch with patches applied on
    top of a remote branch, and push it with a tag to the kernelci Github
    linux repository.  This is meant to be used for branches to test on
    staging.kernelci.org and for testing production updates on
    kernelci.org.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

---
+++ kernel.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2019 Collabora Limited
+# Author: Guillaume Tucker <guillaume.tucker@collabora.com>
+#
+# This module is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import argparse
+import os
+import sys
+
+import kernelci
+from kernelci import print_color, shell_cmd
+
+
+def add_date_commit(path, msg, fname):
+    with open(os.path.join(path, fname), 'w') as f:
+        f.write(msg)
+    shell_cmd("""
+cd {path}
+git add {fname}
+git commit --author='"kernelci.org bot" <bot@kernelci.org>' -am {msg}
+""".format(path=path, fname=fname, msg=msg))
+
+
+def main(args):
+    path = os.path.join('checkout', args.project)
+    repo_name = '/'.join([args.namespace, args.project])
+    repo = kernelci.GITHUB.get_repo(repo_name)
+    kernelci.checkout_repository(path, repo, args.from_url, args.from_branch)
+    patches_path = os.path.join('patches', args.project)
+    if not kernelci.apply_patches(path, patches_path):
+        print_color('red', "Aborting, all patches must apply.")
+        return False
+    tag = args.tag or kernelci.date_tag()
+    add_date_commit(path, tag, args.branch)
+    kernelci.create_tag(path, tag)
+    if args.push:
+        print("\nPushing tag ({}) and branch ({})".format(tag, args.branch))
+        kernelci.push_tag_and_branch(path, args.ssh_key, args.branch, tag)
+    else:
+        print("\nTag: {}".format(tag))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("\
+Create a kernel branch to test KernelCI")
+    parser.add_argument("--tag",
+                        help="Tag to create, default is to use current date")
+    parser.add_argument("--branch", default="staging.kernelci.org",
+                        help="Name of the branch to force-push to")
+    parser.add_argument("--from-url", default=kernelci.TORVALDS_GIT_URL,
+                        help="URL to pull from")
+    parser.add_argument("--from-branch", default="master",
+                        help="Branch to pull from")
+    parser.add_argument("--namespace", default='kernelci',
+                        help="Github project namespace")
+    parser.add_argument("--project", default='linux',
+                        help="Name of the Github project")
+    parser.add_argument("--ssh-key",
+                        help="Path to SSH key to push branches and tags")
+    parser.add_argument("--push", action="store_true",
+                        help="Push the resulting branch and tag")
+    args = parser.parse_args(sys.argv[1:])
+    main(args)
