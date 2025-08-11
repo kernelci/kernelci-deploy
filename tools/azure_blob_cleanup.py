1a47b372b7bda71473ac8ea82f3170570033d286
Denys Fedoryshchenko <denys.f@collabora.com>
2025-08-11 12:09:25 +0300

    Azure cleanup script minor fixes
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>

--- tools/azure_blob_cleanup.py
+++ tools/azure_blob_cleanup.py
@@ -19,6 +19,9 @@ def load_credentials(config_file):
     config = toml.load(config_file)
     return config["azure"]["account"], config["azure"]["key"]
 
+def load_container(config_file, container_name=None):
+    config = toml.load(config_file)
+    return config["azure"].get("container", container_name)
 
 def retrieve_containers(blob_service_client):
     containers = []
@@ -51,19 +54,27 @@ def process_blobs(container_client):
 def main():
     parser = argparse.ArgumentParser(description="Azure Blob Storage Cleanup")
     parser.add_argument("--config", required=True, help="Path to the config file")
-    parser.add_argument("--container", required=True, help="Name of the container to clean up")
+    parser.add_argument("--container", required=False, help="Name of the container to clean up")
     args = parser.parse_args()
 
     storage_account, storage_key = load_credentials(args.config)
+    default_container = load_container(args.config, args.container)
+    if not default_container and not args.container:
+        print("Default container not defined")
+        os.exit(2)
+
     blob_service_client = BlobServiceClient(account_url=f"https://{storage_account}.blob.core.windows.net", credential=storage_key)
+    print("Retrieving containers...")
     containers = retrieve_containers(blob_service_client)
-    if not args.container or not args.container in containers:
+    if not default_container or not default_container in containers:
         print(f"Container '{args.container}' not found. Available containers:")
         for container in containers:
             print(f"- {container}")
         sys.exit(1)
+    print(f"Using container: {default_container}")
 
-    container_client = blob_service_client.get_container_client(args.container)
+    container_client = blob_service_client.get_container_client(default_container)
+    print(f"Processing blobs in container: {default_container}")
     process_blobs(container_client)
 
 
